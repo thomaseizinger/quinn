0b961822954130b538b57730923ea7ac90eefcd2
Thomas Eizinger <thomas@eizinger.io>
2025-04-15 09:09:48 +1000

    Use log instead

--- quinn-udp/src/cmsg/mod.rs
+++ quinn-udp/src/cmsg/mod.rs
@@ -52,7 +52,7 @@ impl<'a, M: MsgHdr> Encoder<'a, M> {
             self.hdr.control_len()
         );
 
-        dbg!(level, ty, space, self.hdr.control_len(), self.len);
+        crate::log::debug!(%level, %ty, %space, control_len = %self.hdr.control_len(), len = %self.len);
 
         let cmsg = self.cmsg.take().expect("no control buffer space remaining");
         cmsg.set(
